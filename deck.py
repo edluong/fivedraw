01a823f3bc9778ea49c6fbe7c2457d01d68ea1f4
edluong <edmundluong@gmail.com>
2019-12-03 22:01:22 -0800

    clean up comments

--- deck.py
+++ deck.py
@@ -5,17 +5,8 @@ class Deck:
     suit = ['heart','club','spade','diamond']
 
     def __init__(self):
-
-#        # fill up the deck
-#        deck = []
-#
-#        for rank in range(1,14):
-#            for suit in self.suit:
-#                self.deck.append(Card(rank,suit))
-#        
         self.deck = [Card(rank,suit) for rank in range(1,14) for suit in self.suit]
 
-    
     def print_deck(self):
         for card in self.deck:
             print(card,end="\n")
