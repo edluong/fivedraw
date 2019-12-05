abb21d26dca401e9772ff90d929128f05ad476f7
edluong <edmundluong@gmail.com>
2019-12-04 22:34:32 -0800

    refactor load_deck

--- deck.py
+++ deck.py
@@ -5,12 +5,12 @@ class Deck:
 
     suit = ['heart','club','spade','diamond']
 
-    def __init__(self):
-        self.deck = [Card(rank,suit) for rank in range(1,14) for suit in self.suit]
-
     def load_deck(self):
         return [Card(rank,suit) for rank in range(1,14) for suit in self.suit]
- 
+    
+    def __init__(self):
+        self.deck = self.load_deck()
+
     def print_deck(self):
         for card in self.deck:
             print(card,end="\n")
