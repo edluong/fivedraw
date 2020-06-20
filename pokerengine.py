c28e34de2f2b132a1331a65bd947cce43aaa8eed
Ed <edmundluong@gmail.com>
2020-06-20 15:16:47 -0700

    separate out hand rank logic

--- pokerengine.py
+++ pokerengine.py
@@ -1,5 +1,3 @@
-# TODO - refactor this code
-
 def _isStraight(rank_list):
     ''' 
     sorts the rank list in order
@@ -16,6 +14,60 @@ def _isStraight(rank_list):
 
     return sorted(rank_list) == recreate_rank_list or sorted(rank_list) == STRAIGHT_WHEEL
 
+def _straight_and_or_flush(rank_list, suit_list):
+    result = None
+
+    # checking if all the suit values are the same
+    if len(set(suit_list)) == 1:
+        result = 'straight flush' if _isStraight(rank_list) else 'flush'
+    else:
+        result = 'straight' if _isStraight(rank_list) else 'nothing'
+    return result
+
+
+def _hand_rank(hand):
+    '''
+    evaluates a hand of five cards and returns a ranking 
+    '''
+    result = None
+    suits = [suit for rank,suit in hand]
+    ranks = [rank for rank,suit in hand]
+    
+    # need to get the counts of each element
+    # compare the counts of the highest groups
+    # finding "pairs" algorithm
+    rank_counts = {}
+    for rank in ranks:
+        if ranks.count(rank) > 1:
+            rank_counts[rank] = ranks.count(rank)
+    
+    _pair_results = {
+        2: 'pair',
+        3: 'trips',
+        4: 'quads'
+    }
+
+    # only one "pair" type is in the dict
+    if len(rank_counts) == 1:
+        # taking the max because there is only one value
+        result = _pair_results[max(rank_counts.values())]
+    # two different types of "pairs"
+    elif len(rank_counts) == 2:
+        # checking if pair dict values contain a 2 and 3 
+        # in other words, need 3 of a kind AND a pair
+        # https://stackoverflow.com/a/6159329/4376173
+        FULL_HOUSE_PAIR_COMBO = [2,3]
+        if all(pair_count in list(rank_counts.values()) for pair_count in FULL_HOUSE_PAIR_COMBO):
+            result = 'full house'
+        elif 2 in set(rank_counts.values()):
+            result = 'two pair'
+        else:
+            raise Exception('calculation error for full house or two pair')
+    else:
+        result = _straight_and_or_flush(ranks, suits)
+    return result
+
+
 def winninghand(players):
     '''
     Takes an array of players hands and finds the best hand
@@ -25,44 +77,4 @@ def winninghand(players):
     for hand in enumerate(players,1):
         # label the players with numbers
         player_no, cards = hand
-                
-        suits = [suit for rank,suit in cards]
-        ranks = [rank for rank,suit in cards]
-      
-        # need to get the counts of each element
-        # compare the counts of the highest groups
-        # finding "pairs" algorithm
-        rank_counts = {}
-        for rank in ranks:
-            if ranks.count(rank) > 1:
-                rank_counts[rank] = ranks.count(rank)
-        
-        _pair_results = {
-            2: 'pair',
-            3: 'trips',
-            4: 'quads'
-        }
-
-        # only one "pair" type is in the dict
-        if len(rank_counts) == 1:
-            # taking the max because there is only one value
-            result = _pair_results[max(rank_counts.values())]
-        # two different types of "pairs"
-        elif len(rank_counts) == 2:
-            # checking if pair dict values contain a 2 and 3 
-            # in other words, need 3 of a kind AND a pair
-            # https://stackoverflow.com/a/6159329/4376173
-            FULL_HOUSE_PAIR_COMBO = [2,3]
-            if all(pair_count in list(rank_counts.values()) for pair_count in FULL_HOUSE_PAIR_COMBO):
-                result = 'full house'
-            elif 2 in set(rank_counts.values()):
-                result = 'two pair'
-            else:
-                raise Exception('calculation error for full house or two pair')
-        else:
-            # checking if all the suit values are the same
-            if len(set(suits)) == 1:
-                result = 'straight flush' if _isStraight(ranks) else 'flush'
-            else:
-                result = 'straight' if _isStraight(ranks) else 'nothing'
-    return result
+        return _hand_rank(cards)
